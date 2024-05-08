31dc3ff42a571c557736d34ddf4a3133e4c0aeac
Jeison Manuyama <jeisonmanuyama@gmail.com>
2024-05-08 03:56:16 -0500

    list details

---
+++ app/Http/Controllers/DetailController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Detail;
+use Illuminate\Http\Request;
+
+class DetailController extends Controller
+{
+    public function index()
+    {
+        $details = Detail::all();
+        return view('details.index', ['details' => $details]);
+    }
+
+    public function create()
+    {
+        return view('details.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'invoice_id' => 'required|exists:invoices,id',
+            'product_id' => 'required|exists:products,id',
+            'quantity' => 'required',
+            'price' => 'required'
+        ]);
+
+        $detail = Detail::create($validated);
+        return redirect()->route('details.index');
+    }
+
+    public function show(Detail $detail)
+    {
+        return view('details.show', ['detail' => $detail]);
+    }
+
+    public function edit(Detail $detail)
+    {
+        return view('details.edit', ['detail' => $detail]);
+    }
+
+    public function update(Request $request, Detail $detail)
+    {
+        $validated = $request->validate([
+            'invoice_id' => 'required|exists:invoices,id',
+            'product_id' => 'required|exists:products,id',
+            'quantity' => 'required',
+            'price' => 'required'
+        ]);
+
+        $detail->update($validated);
+        return redirect()->route('details.index');
+    }
+
+    public function destroy(Detail $detail)
+    {
+        $detail->delete();
+        return redirect()->route('details.index');
+    }
+}
